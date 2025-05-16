059d2feb0826a530ab5563d7ffef7a294a5b2333
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2025-05-16 15:45:34 +0200

    form terminado

--- UD8_DespliegueDeModelos/prediccionBancaria.py
+++ UD8_DespliegueDeModelos/prediccionBancaria.py
@@ -14,14 +14,22 @@ app.secret_key = 'clave_secreta'  # Requisito para CSRF
 csrf = CSRFProtect(app)
 
 class BbvaForm(FlaskForm):
-    ingresos = StringField('income', validators=[DataRequired()])
-    cantidad = StringField('amount', validators=[DataRequired()])
-    tasaIntPres = StringField('int_rate', validators=[DataRequired()])
-    mppIngresp = StringField('percent_income', validators=[DataRequired()])
-    prestAnt = StringField('previous_loans', validators=[DataRequired()])
-    hipoPro = StringField('ownership_MORTGAGE', validators=[DataRequired()])
-    alqPro = StringField('ownership_RENT', validators=[DataRequired()])
-    enviar = SubmitField('enviar')
+    ingresos = StringField('income', validators=[DataRequired()],
+                           render_kw={"placeholder": "15.000 – 150.000"})
+    cantidad = StringField('amount', validators=[DataRequired()],
+                           render_kw={"placeholder": "1.000 – 100.000"})
+    tasaIntPres = StringField('int_rate', validators=[DataRequired()],
+                              render_kw={"placeholder": "5.5 - 13.0 - 20.0"})
+    mppIngresp = StringField('percent_income', validators=[DataRequired()],
+                             render_kw={"placeholder": "15.0, 30.0, 45.0"})
+    prestAnt = StringField('previous_loans', validators=[DataRequired()],
+                           render_kw={"placeholder": "0 = sin impagos, 1 = con impagos"})
+    hipoPro = StringField('ownership_MORTGAGE', validators=[DataRequired()],
+                          render_kw={"placeholder": "0 = renta, 1 = propio, 2 = hipoteca"})
+    alqPro = StringField('ownership_RENT', validators=[DataRequired()],
+                         render_kw={"placeholder": "0 = no tiene, 1 = tiene alquiler"})
+    enviar = SubmitField('Enviar')
+
 
 
 @app.route('/', methods=['GET','POST'])
@@ -49,5 +57,7 @@ def procesar():
         return render_template('banco.html', form=form)
     except Exception as e:
         return f"Error en la predicción: {str(e)}"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
